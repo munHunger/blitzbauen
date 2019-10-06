83fa7ba45e74413ac700bd1a683f3ebe03a2d2fb
munHunger <marcusmunger@hotmail.com>
2019-10-06 10:02:24 +0200

    base util of builder reworked

--- server/src/builder/builder.js
+++ server/src/builder/builder.js
@@ -1,10 +1,23 @@
 const datason = require("datason");
 const fs = require("fs");
+const simplegit = require("simple-git")();
 
-datason.connect("./data").then(db => {
-  function readSettings(repo) {
-    return db.settings;
-  }
-
-  module.exports = { readSettings };
+module.exports = datason.connect("./data").then(db => {
+  return {
+    /**
+     * @param {String} repo The name of the repository
+     */
+    readSettings: repo => db.settings.repositories.find(r => r.name === repo),
+    cloneRepo: repo =>
+      new Promise((resolve, reject) =>
+        simplegit.clone(repo.url, `repos/${repo.name}`).exec(() => {
+          console.log("cloned");
+          if (fs.existsSync(`repos/${repo.name}/blitz.json`))
+            return fs.promises
+              .readFile(`repos/${repo.name}/blitz.json`)
+              .then(data => JSON.parse(data));
+          else reject("Not a blitz project");
+        })
+      )
+  };
 });
--- server/src/builder/builder.spec.js
+++ server/src/builder/builder.spec.js
@@ -1,6 +1,99 @@
-const builder = require("./builder");
+const proxyquire = require("proxyquire").noCallThru();
+const sinon = require("sinon");
+var simplegit = {
+  clone: sinon.stub().returns({
+    exec: fn => {
+      fn.apply();
+    }
+  })
+};
 
-describe("Reading settings", () => {
-  it("rejects if there is no settings file", () =>
-    expect(builder.readSettings("")).not.toBeDefined());
+const settings = {
+  settings: {
+    repositories: [
+      {
+        name: "blitz",
+        url: "github.com/blitz.git",
+        id: "asdf"
+      }
+    ]
+  }
+};
+
+const blitz = {
+  steps: [
+    {
+      name: "test",
+      script: "cd server && npm run test",
+      output: {
+        reports: {
+          test: {
+            dir: "server/test",
+            type: "junit"
+          }
+        }
+      }
+    }
+  ]
+};
+
+const fs = {
+  existsSync: sinon.stub(),
+  promises: {
+    readFile: sinon.stub().returns(new Promise((resolve, _) => resolve(blitz)))
+  }
+};
+
+var datason = {
+  connect: sinon.stub().returns(
+    new Promise((resolve, _) => {
+      resolve(settings);
+    })
+  )
+};
+
+var builder;
+
+describe("Builder", () => {
+  beforeAll(() =>
+    proxyquire("./builder", {
+      fs,
+      datason,
+      "simple-git": sinon.stub().returns(simplegit)
+    }).then(b => (builder = b))
+  );
+  describe("Reading settings", () => {
+    it("returns undefined if no repo is found", () =>
+      expect(builder.readSettings("")).not.toBeDefined());
+    it("returns the repo if found", () =>
+      expect(builder.readSettings("blitz")).toEqual(
+        settings.settings.repositories[0]
+      ));
+  });
+  describe("Cloning repository", () => {
+    it("clones the url and rejects non blitz projects", () => {
+      fs.existsSync.returns(false);
+      builder
+        .cloneRepo(settings.settings.repositories[0])
+        .then(_ => new Error("did not reject"))
+        .catch(_ =>
+          sinon.assert.calledWith(
+            simplegit.clone,
+            settings.settings.repositories[0].url,
+            `repos/${settings.settings.repositories[0].name}`
+          )
+        );
+    });
+    it("clones the url and resolves valid blitz projects", () => {
+      fs.existsSync.returns(true);
+      builder.cloneRepo(settings.settings.repositories[0]).then(data => {
+        sinon.assert.calledWith(
+          simplegit.clone,
+          settings.settings.repositories[0].url,
+          `repos/${settings.settings.repositories[0].name}`
+        );
+        expect(data).toEqual(blitz);
+      });
+    });
+  });
 });
