3ebc0549672df492e401625e3bea938f410133ac
munHunger <marcusmunger@hotmail.com>
2019-10-12 14:00:47 +0200

    removed console log

--- server/src/resolvers/query/index.js
+++ server/src/resolvers/query/index.js
@@ -15,22 +15,6 @@ const history = async (_, input) => {
   let start = (input.page || 0) * size;
   let end = start + size;
   let sortField = (input.sort || {}).field || "timestamp";
-  console.log(
-    Object.keys(db.history)
-      .filter(key => typeof db.history[key] !== "function")
-      .map(key => db.history[key])
-      .sort(
-        (a, b) =>
-          (a[sortField] > b[sortField]
-            ? 1
-            : a[sortField] < b[sortField]
-            ? -1
-            : 0) * ((input.sort || {}).asc ? -1 : 1)
-      )
-      .filter(job => filter(input, job))
-      .slice(start, end)
-      .map(job => historyTransformer(job))
-  );
   return Object.keys(db.history)
     .filter(key => typeof db.history[key] !== "function")
     .map(key => db.history[key])
